b23df164cf8abe7a3ce989ec7bf999ac62b7f992
Louiza <123575767+1oui2a@users.noreply.github.com>
2024-03-06 09:24:12 +0000

    Add files via upload

---
+++ INSERT_Vehicle_Database.php
@@ -0,0 +1,48 @@
+ <?php
+$servername = "localhost";
+$username = "root";
+$password = "";
+$dbname = "Vehicle_Database";
+
+// Create connection
+$conn = mysqli_connect($servername, $username, $password, $dbname);
+// Check connection
+if (!$conn) {
+    die("Connection failed: " . mysqli_connect_error());
+}
+
+// Escape user inputs for security;
+
+$REG_NUMBER = $_POST['REG_NUMBER'];
+
+$CURRENT_MILEAGE = $_POST['CURRENT_MILEAGE'];
+
+$DATE_MADE = $_POST['DATE_MADE'];
+
+$DATE_NCT_DUE = $_POST['DATE_NCT_DUE'];
+
+$VEHICLE_MODEL = $_POST['VEHICLE_MODEL'];
+
+ 
+
+// attempt insert query execution
+
+mysqli_query($conn, "INSERT INTO vehicles (REG_NUMBER, CURRENT_MILEAGE, DATE_MADE, DATE_NCT_DUE, VEHICLE_MODEL) VALUES ('$REG_NUMBER', '$CURRENT_MILEAGE', '$DATE_MADE, $DATE_NCT_DUE, $VEHICLE_MODEL')");
+
+if(mysqli_affected_rows($conn)>0){
+
+    echo "Records added successfully.";
+
+} else{
+
+    echo "ERROR: Could not able to execute $sql. " . mysqli_error($conn);
+
+}
+
+ 
+
+// close connection
+
+mysqli_close($conn);
+
+?>
